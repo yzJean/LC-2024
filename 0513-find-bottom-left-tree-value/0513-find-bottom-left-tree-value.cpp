485b1fd6a4f3e543fbe52abeb5102de88be8b0af
jeanlin <59329465+yzJean@users.noreply.github.com>
2024-07-31 18:53:17 +0800

    Time: 13 ms (60.21%), Space: 23.3 MB (64.16%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -10,32 +10,24 @@
  * };
  */
 class Solution {
+private:
+    void Dfs(TreeNode* node, int curr_depth, int& ans, int& max_depth) {
+        if (!node->left && !node->right) {
+            if (curr_depth > max_depth) {
+                max_depth = curr_depth;
+                ans = node->val;
+            }
+            return;
+        }
+
+        if (node->left) Dfs(node->left, curr_depth + 1, ans, max_depth); // backtracking: curr_depth
+        if (node->right) Dfs(node->right, curr_depth + 1, ans, max_depth); // backtracking: curr_depth
+    }
 public:
     int findBottomLeftValue(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
         int ans = root->val;
-        while(!q.empty()) {
-            size_t s = q.size();
-            bool is_last_row = true;
-            for (size_t i = 0; i < s; i++) {
-                auto node = q.front();
-                q.pop();
-                if (i == 0) ans = node->val;
-                is_last_row = is_last_row && (!node->right && !node->left);
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            if (is_last_row) {
-                break;
-            }
-        };
+        int max_depth = INT_MIN;
+        Dfs(root, 0, ans, max_depth);
         return ans;
     }
 };
-/*
-queue<TreeNode*> q: 2
-
-
-
-*/
